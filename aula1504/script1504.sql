7a868dcccc3b543108e4b5c6929c1b08291da3b8
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-04-16 21:38:54 -0400

    Pesquisa com chaves encadeada e Inner Join

---
+++ aula1504/script1504.sql
@@ -0,0 +1,11 @@
+use empregados_db;
+select * from empregado where cod_depto in(
+	select cod_depto from departamento where cod_depto <  5
+
+);
+
+/* Calculo Com Datas*/
+
+select * from empregado where cod_emp in(
+	select cod_emp from dependente where floor(datediff(now(), dt_nascimento) / 365)  < 18
+);
